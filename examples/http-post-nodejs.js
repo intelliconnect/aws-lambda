6d146ade38cd561179692e8c9a7495427755b03d
ShivanjaliN <shiva1296@gmail.com>
2020-12-07 10:21:02 +0530

    first commit

---
+++ examples/http-post-nodejs.js
@@ -0,0 +1,28 @@
+const https = require('https'); // or https 
+
+const defaultOptions = {
+    host: 'example.com',
+    port: 443, // or 80 for http
+    headers: {
+        'Content-Type': 'application/json',
+    }
+}
+
+const post = (path, payload) => new Promise((resolve, reject) => {
+    const options = { ...defaultOptions, path, method: 'POST' };
+    const req = https.request(options, res => {
+        let buffer = "";
+        res.on('data', chunk => buffer += chunk)
+        res.on('end', () => resolve(JSON.parse(buffer)))
+    });
+    req.on('error', e => reject(e.message));
+    req.write(JSON.stringify(payload));
+    req.end();
+})
+
+
+// Example usage
+exports.handler = async (event, context) => new Promise( async (resolve, reject) => {
+    const token = await post('/auth/login', event); //event will be the payload required to send
+    console.log(token)
+})
